901cd5dac29ebe4edc18f3848ac9a5bd55a198ba
nishaque <61164463+nishaque@users.noreply.github.com>
2021-01-09 11:30:26 +0300

    Update ncstack.hpp

--- include/ncstack.hpp
+++ include/ncstack.hpp
@@ -1,76 +1,130 @@
 // Copyright 2020 Yulia Timoshenko <timoshenkojulie01@gmail.com>
-#ifndef INCLUDE_NCSTACK_HPP
-#define INCLUDE_NCSTACK_HPP
-#include <iostream>
-#include <stdexcept>
-#include <utility>
+#ifndef INCLUDE_SHAREDPTR_HPP
+#define INCLUDE_SHAREDPTR_HPP
 
-#include "Node.hpp"
+#include <atomic>
+#include <iostream>
 
 template <typename T>
-class ncstack {
+class SharedPtr {
  public:
-  void push(T &&value);
-  void push(const T &value);
-  void pop();
-  const T &head() const;
+  SharedPtr() = default;
+  explicit SharedPtr(T* ptr);
+  SharedPtr(const SharedPtr& r);
+  SharedPtr(SharedPtr&& r) noexcept;
+  ~SharedPtr();
+  auto operator=(const SharedPtr& r) -> SharedPtr&;
+  auto operator=(SharedPtr&& r) noexcept -> SharedPtr&;
 
-  ncstack() = default;
-  ncstack(ncstack &) = delete;
-  ncstack(ncstack &&);
+  explicit operator bool() const;
+  auto operator*() const -> T&;
+  auto operator->() const -> T*;
 
-  auto operator=(ncstack &) -> ncstack & = delete;
-  auto operator=(ncstack &&) -> ncstack &;
-
-  ~ncstack();
-
+  auto get() -> T*;
+  void reset();
+  void reset(T* ptr);
+  void swap(SharedPtr& r);
+  auto use_count() const -> size_t;
 
  private:
-  Node<T> *top;
+  T* pointer = nullptr;
+  std::atomic_uint* counter = nullptr;
 };
 
 template <typename T>
-ncstack<T>::~ncstack<T>() {
-  while (top != nullptr) pop();
+SharedPtr<T>::SharedPtr(T* ptr) {
+  pointer = ptr;
+  counter = new std::atomic_uint;
+  *counter = 1;
 }
 
 template <typename T>
-void ncstack<T>::push(T &&value) {
-  top = std::move(new Node<T>{std::forward<T>(value), top});
+SharedPtr<T>::SharedPtr(const SharedPtr& r) {
+  counter = r.counter;
+  pointer = r.pointer;
+  if (counter) ++*counter;
 }
-template <typename T>
-void ncstack<T>::push(const T &value) {
-  top = std::move(new Node<T>{value, top});
-}
-template <typename T>
-void ncstack<T>::pop() {
-  if (top == nullptr) {
-    throw std::out_of_range("Stack is empty!");
-  } else {
-    auto *newtop = top;
-    top = top->prevNode;
-    delete newtop;
 
+template <typename T>
+SharedPtr<T>::SharedPtr(SharedPtr&& r) noexcept {
+  pointer = std::move(r.pointer);
+  r.pointer = nullptr;
+  counter = std::move(r.counter);
+  r.counter = nullptr;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator=(const SharedPtr& r) -> SharedPtr& {
+  if (&r != this) {
+    this->~SharedPtr();
+    pointer = r.pointer;
+    counter = r.counter;
+    if (counter) ++*counter;
+    return *this;
+  } else
+    return *this;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator=(SharedPtr&& r) noexcept -> SharedPtr& {
+  if (&r != this) {
+    this->~SharedPtr();
+    pointer = r.pointer;
+    counter = r.counter;
+    r.pointer = nullptr;
+    r.counter = nullptr;
+    return *this;
+  } else
+    return *this;
+}
+
+template <typename T>
+SharedPtr<T>::operator bool() const {
+  return (bool)pointer;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator*() const -> T& {
+  return *pointer;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator->() const -> T* {
+  return pointer;
+}
+
+template <typename T>
+auto SharedPtr<T>::get() -> T* {
+  return pointer;
+}
+
+template <typename T>
+void SharedPtr<T>::reset() {
+  *this = std::move(SharedPtr<T>());
+}
+
+template <typename T>
+void SharedPtr<T>::reset(T* r) {
+  *this = std::move(SharedPtr<T>(r));
+}
+
+template <typename T>
+void SharedPtr<T>::swap(SharedPtr<T>& r) {
+  std::swap(pointer, r.pointer);
+  std::swap(counter, r.counter);
+}
+
+template <typename T>
+auto SharedPtr<T>::use_count() const -> size_t {
+  return counter ? (size_t)*counter : 0;
+}
+
+template <typename T>
+SharedPtr<T>::~SharedPtr() {
+  if (counter && !--(*counter)) {
+    delete pointer;
+    delete counter;
   }
 }
-template <typename T>
-const T &ncstack<T>::head() const {
-  if (!top) throw std::out_of_range("Stack is empty!");
-  return top->data;
-}
-template <typename T>
-ncstack<T>::ncstack(ncstack &&s) {
-  top = s.top;
-  s.top = nullptr;
-}
-template <typename T>
-auto ncstack<T>::operator=(ncstack &&s) -> ncstack & {
-  if (&s != this) {
-    this->~ncstack();
-    top = s.top;
-    s.top = nullptr;
-  }
-  return *this;
-}
 
-#endif  // INCLUDE_NCSTACK_HPP
+#endif  // INCLUDE_SHAREDPTR_HPP
